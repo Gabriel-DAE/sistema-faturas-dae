3d4dd3162f052effe7b046c51399329410b0bd7b
Gabriel-DAE <gabrielhi@daebauru.sp.gov.br>
2026-04-09 10:01:04 -0300

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -1060,7 +1060,7 @@ with aba4:
                 "demanda_contratada_fponta": [275.0, 0.0]
             })
             
-            st.dataframe(df_faturas.astype(str))
+            st.dataframe(df_exemplo, hide_index=True, use_container_width=True)
             
             arquivo_ucs_excel = st.file_uploader("Selecione a planilha de UCs (.xlsx)", type=["xlsx"], key="upload_uc")
             
